970f2707d412c4ab6dbaaefb2fbafa63cc558bac
narolfo <macanico@gmail.com>
2015-02-02 21:17:19 -0300

    Added APT module and base packages installation

--- cassandratest/modules/cassandra/manifests/init.pp
+++ cassandratest/modules/cassandra/manifests/init.pp
@@ -12,4 +12,30 @@
 #
 class cassandra {
 
+
+#define the base repo
+
+  apt::source { 'cassandra':
+  location   => 'http://debian.datastax.com/community',
+  repos      => 'main',
+  key        => 'http://debian.datastax.com/debian/repo_key',
+  key_server => 'pgp.mit.edu',
+  }
+
+
+#install the needed packages
+    package { 'python-cql':
+        ensure => installed,
+    }
+    
+    package { 'dsc20=2.0.11-1':
+        ensure => installed,
+    } 
+    
+    package { 'cassandra=2.0.11':
+        ensure => installed,
+    }
+    
+    
+
 }
